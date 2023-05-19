750a9f30d17132114508108b22c0bb93cb8aea8f
yiahmed <yahyahmed28@gmail.com>
2023-05-19 16:00:09 -0700

    float animation to image

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       keyframes: {
         'flash-green': {
@@ -20,10 +19,16 @@ module.exports = {
           '0%, 30%, 70%, 100%': { color: 'black' },
           '40%, 60%': { color: 'red' },
         },
+        'floatAnimation': {
+          '0%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
       },
       animation: {
         'flash-green': 'flash-green 5s',
         'flash-red': 'flash-red 5s',
+        'float': 'floatAnimation 3s infinite ease-in-out',
       },
     },
   },
