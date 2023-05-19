53194896f9c60110be4c102add5eede7ddde761f
yiahmed <yahyahmed28@gmail.com>
2023-05-19 11:49:42 -0700

    initial skeleton of counter app, no styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import { AppProps } from 'next/app';
-import Layout from '../components/layout';
-import 'css/globals.css';
+import { AppProps } from "next/app";
+import Layout from "../components/layout";
+import "css/globals.css";
+import { Provider } from "react-redux";
+import { store } from "@/components/store";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
