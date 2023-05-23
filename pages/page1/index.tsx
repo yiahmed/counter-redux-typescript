ac51dd96bc45e36617e7dacc021fec63be16cc0a
yiahmed <yahyahmed28@gmail.com>
2023-05-22 17:06:22 -0700

    initial player stats, still some bugs

--- pages/page1/index.tsx
+++ pages/page1/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import PlayerStat from "@/components/shared/features/api-calls-components/playerStat";
+import axios from "axios";
 
-type Props = {}
+const Page1 = () => {
+  const [playerIds, setPlayerIds] = useState<number[]>([]);
 
-const Page1 = (props: Props) => {
+  useEffect(() => {
+    const fetchPlayerIds = async () => {
+      try {
+        const response = await axios.get(
+          "https://www.balldontlie.io/api/v1/players"
+        );
+        const players = response.data.data;
+        const ids = players.map((player: any) => player.id);
+        setPlayerIds(ids);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPlayerIds();
+  }, []);
+
   return (
-    <div>Page1</div>
-  )
-}
+    <div className="text-black">
+      <h1>Page 1</h1>
+      {playerIds.map((playerId) => (
+        <PlayerStat key={playerId} playerId={playerId} />
+      ))}
+    </div>
+  );
+};
 
-export default Page1
+export default Page1;
