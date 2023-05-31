235030f341453b82d6cc6e764edf07c50c474837
kennethngyen <kennyboy860@gmail.com>
2023-05-31 15:35:29 -0700

    fixed main teams page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
   ],
   theme: {
     extend: {
+      theme: {
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
