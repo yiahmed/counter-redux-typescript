aa63d0d3d9154cb4ae060d0a2aa43d2f2fe3d0ac
Joel Carr <JoelCarr619@gmail.com>
2023-06-01 15:00:49 -0700

    Finished styling players card

--- pages/page2/[playerId]/index.tsx
+++ pages/page2/[playerId]/index.tsx
@@ -3,6 +3,12 @@ import axios from "axios";
 import { Card } from "@mui/material";
 import { useRouter } from "next/router";
 import { useAppSelector } from "@/components/hooks";
+import { Fjalla_One } from "@next/font/google";
+
+const fjalla_one = Fjalla_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 type IndividualPlayer = {
   teamLogo: string | undefined;
@@ -160,38 +166,37 @@ const Index = () => {
 
   const teamLogo = teamObj ? teamObj.logo : "";
   const teamColor = teamObj ? teamObj.color : "";
-
-  const teamAbbreviation = player?.team.abbreviation;
-
   const gradientColor = `linear-gradient(to bottom, ${teamColor} 0%, rgba(0, 0, 0, 0) 130%)`;
 
   return (
-    <div className="flex items-center justify-center w-full h-full rounded-xl shadow-2xl">
+    <div className="flex items-center justify-center w-full h-full rounded-sm shadow-2xl">
       {player && (
-        <div
-          className="flex flex-col w-1/2 border-2 rounded-xl shadow-2xl h-1/2"
-          style={{ background: gradientColor }}
-        >
-          <div className="flex items-center justify-center text-xl font-bold  text-white  border-b rounded-xl h-1/5 px-5 py-10 ">
+        <div className="flex flex-col w-1/2 border-2 rounded-xl shadow-2xl h 1/2  ">
+          <div
+            style={{ background: teamColor }}
+            className={`flex items-center justify-center text-4xl font-bold ${fjalla_one.className} text-white  border-b rounded-xl h-1/5 px-5 py-2`}
+          >
             {player.first_name + " " + player.last_name}
             {player.team && (
               <img
                 src={teamLogo}
                 alt={player.team.abbreviation}
-                className="w-10 h-10 mx-20 space-x-2"
+                className="w-20 h-20 mx-20 space-x-2 "
               />
             )}
           </div>
           <div style={{ background: gradientColor }}>
-            <ul className="p-4 text-lg fjalla text-white  justify-center items-center flex-col flex">
+            <ul
+              className={`p-4 text-lg  text-white ${fjalla_one.className}  justify-center items-center flex-col flex`}
+            >
               {showHeight()}
               <li className="my-4">POSITION: {player.position ?? "N/A"}</li>
-              <li className="my-4">
-                TEAM: {player.team?.abbreviation ?? "N/A"}
-              </li>
               <li>Points Per Game: {averagePts}</li>
+              <br />
               <li>Assists Per Game: {averageAst}</li>
+              <br />
               <li>Rebounds Per Game: {averageReb}</li>
+              <br />
               <li>Games Played: {totalGames}</li>
             </ul>
           </div>
