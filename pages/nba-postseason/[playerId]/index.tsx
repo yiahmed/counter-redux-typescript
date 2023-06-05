31f13024e77ae66b5e4f4ccaa33eed93799e6733
kennethngyen <kennyboy860@gmail.com>
2023-06-05 16:01:31 -0700

    updated playerCards

--- pages/nba-postseason/[playerId]/index.tsx
+++ pages/nba-postseason/[playerId]/index.tsx
@@ -3,7 +3,9 @@ import axios from "axios";
 import { Card } from "@mui/material";
 import { useRouter } from "next/router";
 import { useAppSelector } from "@/components/hooks";
+import PlayerStat from "@/components/playerStat/PlayerStat";
 import { Fjalla_One } from "@next/font/google";
+
 
 const fjalla_one = Fjalla_One({
   subsets: ["latin"],
@@ -120,13 +122,9 @@ const Index = () => {
 
   const showHeight = () => {
     if (player && player.height_feet && player.height_inches) {
-      return (
-        <li className="my-4">
-          HEIGHT: {player.height_feet} Feet {player.height_inches} Inches
-        </li>
-      );
+      return `${player.height_feet}' ${player.height_inches}"`;
     } else {
-      return <li className="my-4">HEIGHT: N/A</li>;
+      return "N/A";
     }
   };
   const teamObj = Object.values(teamInfoFromRedux).find(
@@ -148,7 +146,7 @@ const Index = () => {
             className={`flex items-center text-4xl font-bold ${fjalla_one.className} text-white  border-b   h-1/5 px-7 py-4 rounded-t-xl`}
           >
             <div className="w-2/3">
-              {player.first_name + " " + player.last_name}
+              {player.first_name.toUpperCase() + " " + player.last_name.toUpperCase()}
             </div>
             <div className="w-1/3 ">
               {player.team && (
@@ -161,19 +159,26 @@ const Index = () => {
             </div>
           </div>
           <div style={{ background: "white", color: "black" }}>
-            <ul
-              className={`p-4 text-lg  text-black ${fjalla_one.className}  justify-center items-center flex-col flex`}
+            <div
+              className={`p-4 text-lg  text-black ${fjalla_one.className} justify-end flex-row flex`}
             >
-              {showHeight()}
-              <li className="my-4">POSITION: {player.position ?? "N/A"}</li>
-              <li className="text-2xl">Points Per Game: {averagePts}</li>
-              <br />
-              <li className="text-2xl">Assists Per Game: {averageAst}</li>
-              <br />
-              <li className="text-2xl">Rebounds Per Game: {averageReb}</li>
-              <br />
-              <li className="text-2xl">Games Played: {totalGames}</li>
-            </ul>
+              <div className="p-5 min-h-full w-1/2 ">
+              <div className="flex justify-center items-center ">
+                <img src="https://cdn.vectorstock.com/i/preview-1x/70/84/default-avatar-profile-icon-symbol-for-website-vector-46547084.jpg"
+                className="h-5/6 w-5/6 object-fill"></img>
+              </div>
+              </div>
+              <div className="">
+              <PlayerStat label="HEIGHT" value={showHeight()} />
+              <PlayerStat label="POSITION" value={player.position ?? "N/A"} />
+              <PlayerStat label="POINTS PER GAME" value={averagePts} />
+              </div>
+              <div className="">
+              <PlayerStat label="ASSISTS PER GAME" value={averageAst} />
+              <PlayerStat label="REBOUNDS PER GAME" value={averageReb} />
+              <PlayerStat label="GAMES PLAYED" value={totalGames} />
+              </div>
+            </div>
           </div>
         </div>
       )}
