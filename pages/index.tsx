2e51058d2a4f05303e045d7ce68d98b217671d47
yahyawick <ya1yah2@gmail.com>
2023-06-21 15:16:50 -0700

    fixed shadow and placing of landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 
 export default function MultiActionAreaCard() {
   return (
@@ -17,8 +17,8 @@ export default function MultiActionAreaCard() {
         minHeight: "90vh",
       }}
     >
-      <Card sx={{ maxWidth: 900 }}>
-        <CardActionArea>
+      <Card sx={{ maxWidth: 900, marginTop: 3 }}>
+        <CardActionArea href={"/nba-postseason"}>
           <CardMedia
             component="img"
             height="full"
@@ -30,7 +30,8 @@ export default function MultiActionAreaCard() {
               Welcome to The Billboard
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              The Crowds are up, their hands are down. Welcome to where the numbers matters now = NBA Post Season 22/23
+              The Crowds are up, their hands are down. Welcome to where the
+              numbers matters now = NBA Post Season 22/23
             </Typography>
           </CardContent>
         </CardActionArea>
@@ -42,13 +43,9 @@ export default function MultiActionAreaCard() {
           display: "flex",
           justifyContent: "center",
           padding: "16px",
-          boxShadow: "20px 20px 0px rgba(0, 0, 0, 0.1)",
         }}
       >
-        <Icon
-          icon="fluent-emoji:basketball"
-          style={{ fontSize: "50px" }}
-        />
+        <Icon icon="fluent-emoji:basketball" style={{ fontSize: "50px" }} />
       </div>
     </div>
   );
